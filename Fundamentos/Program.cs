85aac703167561e78bfb0888cf25c4962eecc23f
star-shield <felipebrandao462@gmail.com>
2025-10-01 21:49:21 -0300

    adicionado variáveis

--- Fundamentos/Program.cs
+++ Fundamentos/Program.cs
@@ -1,7 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 #region boa sorte
 
-    Console.WriteLine("Hello, World!");
+    //Console.WriteLine("Hello, World!");
 
 #endregion
 
+#region Tipos de Variáveis
+    
+    int idade;
+    double altura;
+    string nome;
+    bool estudante;
+
+    Console.Write("Insira seu nome: ");
+    nome = Console.ReadLine()!;
+
+    Console.Write("Insira sua idade: ");
+    idade = int.Parse(Console.ReadLine()!);
+    Console.WriteLine($"Seu nome é {nome} e sua idade é {idade}");
+#endregion
