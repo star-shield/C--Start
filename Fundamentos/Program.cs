2a262e8f0b18a014d0efe75c5506b4c6a9c5ca21
star-shield <felipebrandao462@gmail.com>
2025-10-03 21:54:07 -0300

    exercicios resolvidos

--- Fundamentos/Program.cs
+++ Fundamentos/Program.cs
@@ -1,21 +1,77 @@
 ﻿// See https://aka.ms/new-console-template for more information
 #region boa sorte
 
-    //Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 
 #endregion
 
 #region Tipos de Variáveis
-    
-    int idade;
-    double altura;
-    string nome;
-    bool estudante;
 
-    Console.Write("Insira seu nome: ");
-    nome = Console.ReadLine()!;
-    Console.Write("Insira sua idade: ");
-    idade = int.Parse(Console.ReadLine()!);
+/*
+int idade;
+double altura;
+string nome;
+bool estudante;
+
+Console.Write("Insira seu nome: ");
+nome = Console.ReadLine()!;
+Console.Write("Insira sua idade: ");
+idade = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Seu nome é {nome} e sua idade é {idade}");
+*/
+
+#endregion
+
+#region exercicio1
+
+string nome;
+int idade;
+
+Console.Write("Digite seu nome: ");
+nome = Console.ReadLine()!;
+Console.Write("Digite sua idade: ");
+idade = int.Parse(Console.ReadLine()!);
+
+Console.Write($"Seu nome é {nome} e você tem {idade} anos");
+
+#endregion
+
+#region exercicio2
+
+int a, b;
+
+    Console.Write("Digite o primeiro número: ");
+    a = int.Parse(Console.ReadLine()!);
+    Console.Write("Digite o segundo número: ");
+    b = int.Parse(Console.ReadLine()!);
+
+    Console.WriteLine($"O resultado da soma dos algarismos é: {a+b}");
+    Console.WriteLine($"O resultado da subtração dos algarismos é: {a-b}");
+    Console.WriteLine($"O resultado da multiplicação dos algarismos é: {a*b}");
+    Console.WriteLine($"O resultado da divisão dos algarismos é: {a/b}");
     
-    Console.WriteLine($"Seu nome é {nome} e sua idade é {idade}");
+#endregion
+
+#region exercicio3
+    
+    int raio;
+    const double pi = 3.14;
+
+    Console.Write("Digite o valor do raio: ");
+    raio = int.Parse(Console.ReadLine()!);
+
+    Console.WriteLine($"O resultado da área é {pi * (raio*raio)} e o perímetro é {2*pi*raio}");
+    
+#endregion
+
+#region exercicio4
+
+    int temperatura;
+
+    Console.Write("Digite a temperatura: ");
+    temperatura = int.Parse(Console.ReadLine()!);
+
+    Console.WriteLine($"A conversão da temperatura em Fahreheit é {temperatura *9/5+32}");
+    
 #endregion
