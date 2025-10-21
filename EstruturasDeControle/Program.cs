b04a051470779c70e6f1e26d3b0e9117c68e7ac1
star-shield <felipebrandao462@gmail.com>
2025-10-20 22:02:26 -0300

    nova atividade

--- EstruturasDeControle/Program.cs
+++ EstruturasDeControle/Program.cs
@@ -18,17 +18,75 @@
 
 #region esta choveno
 
-bool estaChovendo = true;
+// bool estaChovendo = true;
 
-Console.Write("Saindo de casa");
-if(estaChovendo)
-{
-    Console.WriteLine("Leve o guarda chuva");
-    Console.Write("Continue o passeio");
-}
-else
-{
-    Console.Write("Continue o passeio");
-}
+// Console.Write("Saindo de casa");
+// if(estaChovendo)
+// {
+//     Console.WriteLine("Leve o guarda chuva");
+//     Console.Write("Continue o passeio");
+// }
+// else
+// {
+//     Console.Write("Continue o passeio");
+// }
 
 #endregion
+
+#region atividade if/else
+
+int temperatura;
+
+Console.Write("Digite a temperatura em celsius: ");
+temperatura = int.Parse(Console.ReadLine()!);
+
+if (temperatura < 0)
+    {
+        Console.Write("temperatura negativa");
+    }
+else if (temperatura <= 30)
+    {
+        Console.Write("temperatura amena");
+    }
+    else
+        {
+            Console.Write("temperatura alta");
+        }
+
+#endregion
+
+#region atividade operador ternério
+
+
+// int numero;
+
+// Console.Write("Digite um número: ");
+// numero = int.Parse(Console.ReadLine()!);
+
+// string mensagem = numero > 0 ? $"{numero} É POSITIVO" : numero < 0 ?$ "{numero} É NEGATIVO": "zero"
+
+// Console.Write(mensagem);
+
+#endregion
+
+#region atividade switch
+
+// char opcao;
+// Console.WriteLine("\n*DIGITE A OPÇÃO ABAIXO*\n");
+// Console.WriteLine("A) Produto alimentício");
+// Console.WriteLine("B) Produto bebida");
+// Console.WriteLine("C) Produto limpeza");
+// Console.WriteLine("D) Produto eletrônico");
+
+// opcao = Console.ReadKey().KeyChar;
+
+// switch (opcao)
+// {
+//     case 'A': Console.Write("\nDescrição do produto alimentício..."); break;
+//     case 'B': Console.Write("\nDescrição das bebidas...") ;break;
+//     case 'C': Console.Write("\nDescrição do produto de limpeza..."); break;
+//     case 'D': Console.Write("\nDescrição do produto eletrônico..."); break;
+//     default: Console.Write("\nCategoria inválida"); break;
+//}
+#endregion
+
