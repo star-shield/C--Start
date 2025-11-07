180e40dad3d0a8a89eff22d8023a28d4bbc10fa6
star-shield <felipebrandao462@gmail.com>
2025-11-06 22:17:30 -0300

    questão 4 e 6

--- EstruturasDeRepeticao/Program.cs
+++ EstruturasDeRepeticao/Program.cs
@@ -7,10 +7,52 @@
 // {
 //     Console.WriteLine($"{numero} x {i} = {numero * i}");
 // }
+#region list
+// List<int> numeros = new List<int>();
+// int quantidade = 0;
+// for (int i =0; i <=10; i++)
+// {
+//     Console.Write("Entre com uma lista de 10 números: ");
+//     numeros.Add(int.Parse(Console.ReadLine()!));
+// }
 
-int a=0, b=1, soma=0, prox=0;
-for (int i = 0; i <= 12; i++)
+
+// foreach (int numero in numeros)
+// {
+//     if (numero % 2 == 0)
+//         quantidade++;
+// }
+// Console.WriteLine($"A quantidade de números pares é: {quantidade}");
+#endregion
+
+#region questão 4
+
+// string[] frutas = { "Maçã", "Banana", "Abacaxi", "Laranja", "Amora" };
+
+
+// foreach (var fruta in frutas)
+// {
+//     if (fruta.StartsWith("A"))
+//         continue;
+//     Console.WriteLine(fruta); 
+// }
+#endregion
+
+#region questão 5
+
+
+
+#endregion
+#region questão 6
+
+int numero;
+
+Console.Write("Entre com um número positivo: ");
+numero = int.Parse(Console.ReadLine()!);
+do
 {
-   
+    Console.WriteLine(numero);
+    numero --;
+} while (numero > -1);
 
-}
+#endregion
