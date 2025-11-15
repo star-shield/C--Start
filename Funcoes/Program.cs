e3d0ea48b4961ad853a4631bff11032c983fbec8
star-shield <felipebrandao462@gmail.com>
2025-11-14 22:12:44 -0400

    código refatorado

--- Funcoes/Program.cs
+++ Funcoes/Program.cs
@@ -1,7 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-void menu()
+﻿void linha()
 {
     Console.WriteLine("--------------------------------------");
+}
+double ConversorDeTemperatura(double temperatura, string OpcaoEscolhida)
+{
+    double resultado=0;
+    switch (OpcaoEscolhida)
+    {
+        case "1":
+            resultado = (temperatura * 9 / 5) + 32;
+            break;
+
+        case "2":
+            resultado = (temperatura - 32) * 5 / 9;
+            break;
+
+        case "3":
+            resultado = temperatura + 273.15;
+            break;
+
+        case "4":
+            resultado = temperatura - 273.15;
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida!");
+            break;
+    }
+    return resultado;
+}
+string continuar;
+do
+{
+    linha();
     Console.WriteLine("Conversor de Temperatura");
     Console.WriteLine("Escolha a opção de conversão:");
     Console.WriteLine("1 - Celsius para Fahrenheit");
@@ -9,54 +40,41 @@ void menu()
     Console.WriteLine("3 - Celsius para Kelvin");
     Console.WriteLine("4 - Kelvin para Celsius");
     Console.Write("Opção: ");
-
     string opcao = Console.ReadLine()!;
-}
-
-int ConversorDeTemperatura(string OpcaoEscolhida)
-{
-    string continuar;
-
-do
-{
-    double temperatura, resultado;
-
-    switch (OpcaoEscolhida)
+    
+    double temperatura;
+    switch (opcao)
     {
         case "1":
-            Console.WriteLine("--------------------------------------");
+            linha();
             Console.Write("Digite a temperatura em Celsius: ");
             temperatura = double.Parse(Console.ReadLine()!);
-            resultado = (temperatura * 9 / 5) + 32;
-            Console.WriteLine($"{temperatura}°C = {resultado:F2}°F");
-            Console.WriteLine("--------------------------------------");
+            Console.WriteLine($"{temperatura}°C = {ConversorDeTemperatura(temperatura, opcao):F2}°F");
+            linha();
             break;
 
         case "2":
-            Console.WriteLine("--------------------------------------");
+            linha();
             Console.Write("Digite a temperatura em Fahrenheit: ");
             temperatura = double.Parse(Console.ReadLine()!);
-            resultado = (temperatura - 32) * 5 / 9;
-            Console.WriteLine($"{temperatura}°F = {resultado:F2}°C");
-            Console.WriteLine("--------------------------------------");
+            Console.WriteLine($"{temperatura}°F = {ConversorDeTemperatura(temperatura, opcao):F2}°C");
+            linha();
             break;
 
         case "3":
-            Console.WriteLine("--------------------------------------");
+            linha();
             Console.Write("Digite a temperatura em Celsius: ");
             temperatura = double.Parse(Console.ReadLine()!);
-            resultado = temperatura + 273.15;
-            Console.WriteLine($"{temperatura}°C = {resultado:F2}K");
-            Console.WriteLine("--------------------------------------");
+            Console.WriteLine($"{temperatura}°C = {ConversorDeTemperatura(temperatura, opcao):F2}K");
+            linha();
             break;
 
         case "4":
-            Console.WriteLine("--------------------------------------");
+            linha();
             Console.Write("Digite a temperatura em Kelvin: ");
             temperatura = double.Parse(Console.ReadLine()!);
-            resultado = temperatura - 273.15;
-            Console.WriteLine($"{temperatura}K = {resultado:F2}°C");
-            Console.WriteLine("--------------------------------------");
+            Console.WriteLine($"{temperatura}K = {ConversorDeTemperatura(temperatura, opcao):F2}°C");
+            linha();
             break;
 
         default:
@@ -68,5 +86,6 @@ do
     continuar = Console.ReadLine()!;
     Console.WriteLine();
 
-} while (continuar.Equals("S", StringComparison.OrdinalIgnoreCase));
-}
+}while(continuar.Equals("S", StringComparison.OrdinalIgnoreCase));
+
+Console.WriteLine("Programa encerrado.");
