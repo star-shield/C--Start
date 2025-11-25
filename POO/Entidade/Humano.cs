16a077b3b1eaf0f7086c6f6e9a834c14203ecb64
star-shield <felipebrandao462@gmail.com>
2025-11-24 22:00:48 -0300

    pasta poo criada

---
+++ POO/Entidade/Humano.cs
@@ -0,0 +1,10 @@
+public class Humano
+{
+    string nome, raca;
+    int idade;
+
+    public void Andar()
+    {
+        Console.WriteLine("Andando...");
+    }
+}
---
+++ POO/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        Humano pessoa = new Humano();
+
+        pessoa.Andar();
+    }
+}
